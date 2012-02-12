148a4ad2fc0c9bf61a7dc47ddbf3a5d25d36f621
Dustin Sallings <dustin@spy.net>
2012-02-12 03:12:26 -0800

    Scheme compiler fixes and stuff.

--- example/example.go
+++ example/example.go
@@ -3,24 +3,22 @@ package main
 import (
 	"image"
 	"image/png"
-	"log"
 	"math/rand"
 	"os"
 
 	"github.com/dustin/go-heatmap"
+	"github.com/dustin/go-heatmap/schemes"
 )
 
 func main() {
 	points := []heatmap.DataPoint{}
-	for n := 0; n < 200; n++ {
+	for n := 0; n < 350; n++ {
 		points = append(points,
 			heatmap.DataPoint{rand.Float64(), rand.Float64()})
 	}
 
-	scheme, err := heatmap.SchemeFromImage("../fire.png")
-	if err != nil {
-		log.Fatalf("Error initializing scheme: %v", err)
-	}
+	// scheme, _ := schemes.FromImage("../schemes/fire.png")
+	scheme := schemes.Fire
 
 	img := heatmap.Heatmap(image.Rect(0, 0, 1024, 1024),
 		points, 150, 128, scheme)
--- schemecompiler/main.go
+++ schemecompiler/main.go
@@ -1,31 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"image/color"
 	"log"
 	"os"
 
 	"github.com/dustin/go-heatmap/schemes"
 )
 
-func main() {
-	log.SetFlags(0)
-	if len(os.Args) < 2 {
-		log.Fatalf("What are you wanting to compile?")
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: schemecompiler [-flags] infile.png\n")
+		flag.PrintDefaults()
 	}
-	scheme, err := schemes.FromImage(os.Args[1])
+}
+
+func main() {
+
+	var pkg = flag.String("package", "example", "scheme's package")
+	var name = flag.String("name", "MyAwesomeScheme", "scheme's name")
+
+	flag.Parse()
+	log.SetFlags(0)
+	if flag.NArg() < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	scheme, err := schemes.FromImage(flag.Arg(0))
 	if err != nil {
 		log.Fatalf("Error initializing scheme: %v", err)
 	}
 
-	fmt.Printf("package whatever\n\n")
+	fmt.Printf("package %s\n\n", *pkg)
 	fmt.Printf("import \"image/color\"\n\n")
-	fmt.Printf("var MyAwesomeScheme = []color.Color{\n")
-	for _, cin := range scheme {
-		c := cin.(color.RGBA)
-		fmt.Printf("\tcolor.RGBA{%v, %v, %v, %v},\n",
-			c.R, c.B, c.G, c.A)
+	fmt.Printf("var %s []color.Color\n\n", *name)
+
+	fmt.Printf("func init() {\n")
+	fmt.Printf("\t%s = []color.Color{\n", *name)
+	for _, c := range scheme {
+		fmt.Printf("\t\t%#v,\n", c)
 	}
-	fmt.Printf("}")
+	fmt.Printf("\t}\n}\n")
 }
